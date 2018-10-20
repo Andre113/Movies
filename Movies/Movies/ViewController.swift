f5e608a852aaadc2bb58faf57eb86efbbd71dc95
André Ota <andre.lucas.ota@gmail.com>
2018-10-19 23:47:32 -0300

    First Commit

---
+++ Movies/Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Andre Lucas Ota on 10/19/18.
+//  Copyright © 2018 AndreOta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
